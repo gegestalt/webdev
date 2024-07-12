04c71d2e34007d4f5a786853bbcadde4f70d9e81
gunal4 <gunal@meikosoft.com>
2024-07-12 12:36:37 +0300

    initialization

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const authRoutes = require('./routes/auth.routes');
+
+
+app.use(authRoutes);
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.listen(3000);
---
+++ controllers/auth.controller.js
@@ -0,0 +1,10 @@
+function getSignup(req,res) {
+    
+}
+function getLogin(req,res){
+    
+}
+module.exports = {
+    getSignup: getSignup,
+    getLogin: getLogin,
+};
