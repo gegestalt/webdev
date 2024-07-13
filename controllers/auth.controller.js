0623e72717efed1eba241838b736358542dde45f
gunal4 <gunal@meikosoft.com>
2024-07-13 22:17:42 +0300

    links on signup page works (except forgot password), login link redirects to recently added login page.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,12 +5,20 @@ function getSignup(req,res) {
     res.render('customer/auth/signup');
 }
 function getLogin(req,res){
+    res.render('customer/auth/login');
     
 }
 function signup(req,res){
     const user = new User(
-        
+        req.body.email,
+        req.body.password,
+        req.body.fullname,
+        req.body.street,
+        req.body.postal,
+        req.body.city
     );
+    user.signup();
+    res.redirect('/login');
 }
 
 module.exports = {
